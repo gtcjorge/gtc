69bf8e37ce02ae4669d2d08799d3f0e0e88e8661
jorge <jorge@globaltrainingcenter.com>
2018-02-07 13:38:42 -0700

    Added gulp, fixed watermark, fixed date range <= 30days

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,44 @@
-const gulp = require("gulp");
-const minify = require("gulp-babel-minify");
+const gulp = require('gulp');
+const minify = require('gulp-babel-minify');
+const tap = require('gulp-tap');
+const rename = require('gulp-rename');
 
-gulp.task("default", () =>
-  gulp.src("./gtctoolkit.user.js")
+const version = 1.8;
+const watermark = `// ==UserScript==
+// @name         GTC Toolkit
+// @namespace    http://www.globaltrainingcenter.com/
+// @version      ${version}
+// @description  Tools
+// @author       Jorge Dominguez
+// @copyright    2017, gtcjorge (https://openuserjs.org/users/gtcjorge)
+// @include      https://na8.salesforce.com/*
+// @require      https://code.jquery.com/jquery-3.3.1.slim.min.js
+// @require      http://globaltrainingcenter.com/date.js
+// @updateURL    @updateURL https://github.com/gtcjorge/gtc/raw/master/gtctoolkit.user.js
+// @downloadURL  @updateURL https://github.com/gtcjorge/gtc/raw/master/gtctoolkit.user.js
+// @connect      www.globaltrainingcenter.com
+// @connect      globaltrainingcenter.com
+// @connect      login.salesforce.com
+// @grant GM_xmlhttpRequest
+// @grant GM_setValue
+// @grant GM_getValue
+// ==/UserScript==
+
+`;
+
+function minifyfile() {
+  return gulp.src('./gtctoolkit-dev.user.js')
     .pipe(minify({
       mangle: {
-        keepClassName: true
-      }
+        keepClassName: true,
+      },
     }))
-    .pipe(gulp.dest("./dist"))
-);
+    .pipe(tap((file) => {
+      file.contents = Buffer.from(watermark + file.contents.toString());
+    }))
+    .pipe(rename('gtctoolkit.user.js'))
+    .pipe(gulp.dest('./'));
+}
+gulp.task(minifyfile);
+
+gulp.task('default', gulp.series(minifyfile));
