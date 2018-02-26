bebebebb3a7209e128875d054923fe34fcc4fa9d
jorge <jorge@globaltrainingcenter.com>
2018-02-26 09:34:24 -0700

    added behavior for wrong password, updated to late february password

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const minify = require('gulp-babel-minify');
 const tap = require('gulp-tap');
 const rename = require('gulp-rename');
 
-const version = 1.8;
+const version = 2.0;
 const watermark = `// ==UserScript==
 // @name         GTC Toolkit
 // @namespace    http://www.globaltrainingcenter.com/
